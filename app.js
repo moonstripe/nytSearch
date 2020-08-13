5384c8635b81ccf8c71ff94abf11e3bec81ba5a9
Kojin Glick <kojinglick@gmail.com>
2020-08-12 21:40:32 -0700

    end of wednesday

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+
 const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=HOZCYgVfQ3HWPU70qBSEjAU8Sw8Yj7AV"
+
+
+$('#searchquery').on('submit', function (event) {
+    event.preventDefault();
+    console.log(event);
+})
 
 $.ajax({
     url: queryURL,
